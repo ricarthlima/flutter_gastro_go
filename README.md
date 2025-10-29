e769ba42b92ecc9eeb216340404b2553d7e2a03e
ricarthlima <ricarth.lima@gmail.com>
2025-10-28 21:15:23 -0300

    chore: flutter created

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_gastro_go
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_gastro_go/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_gastro_go
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
